4d1062aaf21907a13b03ed2c141ac7ec2d5476fb
Maximilian Fickers <m.fickers@basecom.de>
2019-04-03 21:19:14 +0200

    Update .gitignore, documentation in main.c

--- ifa82-reversi/main.c
+++ ifa82-reversi/main.c
@@ -1,6 +1,17 @@
+/**
+ * Our implementation of the game Reversi, written in C.
+ * Project for class IFA82.
+ *
+ * Author: Maximilian Fickers, Julian Görres, Fabian Heeke
+ * Date: 03.04.2019
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Start.
+ **/
 int main()
 {
     printf("Hello world!\n");
