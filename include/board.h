2d466fd149b0513e957c28820fe43060d492caf1
Maximilian Fickers <m.fickers@basecom.de>
2019-04-10 20:16:59 +0200

    Add structs of game and board

---
+++ include/board.h
@@ -0,0 +1,21 @@
+/**
+ * Represents a Reversi board.
+ *
+ * Author: Maximilian Fickers, Julian Görres, Fabian Heeke
+ * Date: 10.04.2019
+ **/
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
+#define EMPTY 0
+#define PLAYER_ONE 1
+#define PLAYER_TWO 2
+
+struct Board {
+    char fields[8][8];
+}
+
+function init_board(struct Board board);
+
+#endif // BOARD_H_INCLUDED
+
